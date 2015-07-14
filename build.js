c54c68df1b8162febaa23745755f7f7f81b74ad9
Kurt Symanzik <kurt@kbsymanzik.org>
2015-07-14 08:38:14 +0800

    Lots of changes.

--- build.js
+++ build.js
@@ -9,20 +9,67 @@ var metalsmith = require('metalsmith'),
     excerpts = require('metalsmith-excerpts'),
     concat = require('metalsmith-concat'),
     copy = require('metalsmith-copy'),
+    less = require('metalsmith-less'),
     moment = require('moment');
 
 // --------------------------------------------------------
 // http://blog.lecomte.me/posts/2014/gentle-intro-metalsmith/way-of-metalsmith/
 // --------------------------------------------------------
 var logFilesMap = function(files, metalsmith, done) {
-    Object.keys(files).forEach(function (file) {
-        var fileObject = files[file];
+  Object.keys(files).forEach(function (file) {
+      var fileObject = files[file];
 
-        if (file.indexOf('posts') === 0) {
-          console.log("key -------> ", file);
-          console.log("value -----> ", fileObject);
+      if (file.indexOf('posts') === 0) {
+        console.log("key -------> ", file);
+        console.log("value -----> ", fileObject);
+      }
+  });
+  done();
+};
+
+// --------------------------------------------------------
+// Assumes that it is working with Markdown so needs to be
+// placed before the Markdown plugin.
+//
+// TODO:
+// 1. Separate out as a separate plugin.
+// 2. Use minimatch for matching patterns.
+// 3. Incorporate tests.
+// --------------------------------------------------------
+var easyImage = function(config) {
+  var pattern = config.pattern || void 0;
+  console.log(pattern);
+
+  return function(files, metalsmith, done) {
+    var getFields = function(keys) {
+      var flds = [];
+      keys.forEach(function(k) {
+        if (k.search(/^IMAGE-/)) flds.push(k);
+      });
+      return flds;
+    };
+
+    Object.keys(files).forEach(function(file) {
+      if (file.indexOf(pattern) !== -1) {
+        var fileObj = files[file];
+        var flds = getFields(Object.keys(fileObj));
+        var val;
+        var contents;
+        if (flds) {
+          console.log(file);
+          console.dir(flds);
+          //contents = fileObj.contents.toString();
+          //flds.forEach(function(fld) {
+            //val = fileObj[fld];
+            //contents = contents.replace(fld, "<img src='" + val + "'>");
+          //});
+          //// Write the finished results back to the node.
+          //fileObj.contents.write(contents);
         }
+      }
     });
+    done();
+  };
 };
 
 var siteBuild = metalsmith(__dirname)
@@ -35,6 +82,10 @@ var siteBuild = metalsmith(__dirname)
   })
   .source('./src')
   .destination('./out')
+  .use(less({
+    // Just my less file, not the vendor's.
+    pattern: 'styles/main.less'
+  }))
   .use(concat({
     files: [
       'bower_components/jquery/dist/jquery.js',
@@ -56,6 +107,9 @@ var siteBuild = metalsmith(__dirname)
     pattern: 'bower_components/bootstrap/dist/fonts/*',
     directory: 'fonts/'
   }))
+  .use(easyImage({
+    pattern: 'posts/*.md'
+  }))
   .use(markdown())
   .use(excerpts())
   .use(collections({
@@ -75,7 +129,7 @@ var siteBuild = metalsmith(__dirname)
     engine: 'jade',
     moment: moment    // make the moment library available to the templates
   }))
-  .use(logFilesMap)
+  //.use(logFilesMap)
   .use(serve({
     port: 9000,
     verbose: true
--- src/posts/test-post.md
+++ src/posts/test-post.md
@@ -4,11 +4,13 @@ publishDate: 2015-05-09
 modifyDate: 2015-05-09
 author: Kurt Symanzik
 template: post.jade
-images: ['IMAGE-01', 'IMAGE-02-SMALL-RIGHT']
+IMAGE-01: images/Coffee-16948426_75x87.jpg
 ---
 
 This is a test post. If this had been an actual post, it might have been
 interesting - VERY INTERESTING oh so interesting!!!
+
+IMAGE-01
 
 - One
 - Two
